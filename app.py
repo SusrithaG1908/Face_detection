5bc0ec314cec620255fb0e6b63a858582d98d244
Susritha Gudimetla <161280408+SusrithaG1908@users.noreply.github.com>
2025-06-26 23:47:45 +0530

    Initial Commit
    Initial Commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade  = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+
+# Start webcam
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame. Exiting …")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        roi_gray  = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
+
+    cv2.imshow('video_live', frame)
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) & 0xFF in (ord('q'), 27):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
